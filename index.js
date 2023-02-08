5c691f9519587014dcfb80f881fedb344e0dfc70
Trezlor <77504889+Trezlor@users.noreply.github.com>
2023-02-08 01:55:04 +0100

    feature: checkboxes

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const characters = 'abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*?';
+const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numbers = '1234567890';
+const symbols = '!@#$%^&*?';
 
-const numberInput = document.getElementById('numberInput');
+const numberInput = document.getElementById('number_input');
+
+const symbolsCheckbox = document.getElementById('symbols_checkbox');
+const numbersCheckbox = document.getElementById('numbers_checkbox');
 
 const passwordOne = document.getElementById('pw_1');
 const passwordTwo = document.getElementById('pw_2');
@@ -16,9 +21,18 @@ function generatePassword() {
 	// TO PREVENT VALUES TO BE < 15 AND > THAN 33, WHILE ALSO SETTING THE VALUE TO CLOSEST OF 15-33 IF THEY ARE OUT OF RANGE
 	const allowedValue = numberInput.value < 6 ? 6 : numberInput.value > 33 ? 33 : numberInput.value;
 	numberInput.value = allowedValue;
-
 	// TO REJECT VALUES OUT OF RANGE
 	if (numberInput.value < 6 || numberInput.value > 33) return;
+
+	// TO LET USER CHOOSE TO HAVE NUMBERS/SYMBOLS INCLUDED
+	const characters =
+		symbolsCheckbox.checked && numbersCheckbox.checked
+			? letters + numbers + symbols
+			: symbolsCheckbox.checked && numbersCheckbox.checked != true
+			? letters + symbols
+			: symbolsCheckbox.checked != true && numbersCheckbox.checked
+			? letters + numbers
+			: letters;
 
 	for (let i = 0; i < numberInput.value; i++) {
 		generatedPasswordOne += characters.charAt(Math.floor(Math.random() * characters.length));
